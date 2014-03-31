b896dd7d7c750357fa3caeb28ef77aac8f7a7d89
Andreas Gruenbacher <agruenba@redhat.com>
2014-04-01 05:29:34 +0530

    vfs: Add permission flags for setting file attributes
    Richacls support permissions that allow to take ownership of a file,
    change the file permissions, and set the file timestamps.  Support that
    by introducing new permission mask flags and by checking for those mask
    flags in setattr_prepare().
    
    Signed-off-by: Andreas Gruenbacher <agruenba@redhat.com>
    Reviewed-by: J. Bruce Fields <bfields@redhat.com>
    Reviewed-by: Steve French <steve.french@primarydata.com>
    Reviewed-by: Jeff Layton <jlayton@redhat.com>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -88,6 +88,9 @@ typedef int (dio_iodone_t)(struct kiocb *iocb, loff_t offset,
 #define MAY_CREATE_DIR		0x00000200
 #define MAY_DELETE_CHILD	0x00000400
 #define MAY_DELETE_SELF		0x00000800
+#define MAY_TAKE_OWNERSHIP	0x00001000
+#define MAY_CHMOD		0x00002000
+#define MAY_SET_TIMES		0x00004000
 
 /*
  * flags in file.f_mode.  Note that FMODE_READ and FMODE_WRITE must correspond
