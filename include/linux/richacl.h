3255cb6f5ddc3471c459750104fa19fafac8c4ba
Andreas Gruenbacher <agruenba@redhat.com>
2014-04-01 18:13:16 +0530

    richacl: Compute maximum file masks from an acl
    Compute upper bound owner, group, and other file masks with as few
    permissions as possible without denying any permissions that the NFSv4
    acl in a richacl grants.
    
    This algorithm is used when a file inherits an acl at create time and
    when an acl is set via a mechanism that does not provide file masks
    (such as setting an acl via nfsd).  When user-space sets an acl via
    setxattr, the extended attribute already includes the file masks.
    
    Setting an acl also sets the file mode permission bits: they are
    determined by the file masks; see richacl_masks_to_mode().
    
    Signed-off-by: Andreas Gruenbacher <agruenba@redhat.com>
    Reviewed-by: J. Bruce Fields <bfields@redhat.com>

--- include/linux/richacl.h
+++ include/linux/richacl.h
@@ -301,5 +301,6 @@ extern void richace_copy(struct richace *, const struct richace *);
 extern int richacl_masks_to_mode(const struct richacl *);
 extern unsigned int richacl_mode_to_mask(mode_t);
 extern unsigned int richacl_want_to_mask(unsigned int);
+extern void richacl_compute_max_masks(struct richacl *, kuid_t);
 
 #endif /* __RICHACL_H */
