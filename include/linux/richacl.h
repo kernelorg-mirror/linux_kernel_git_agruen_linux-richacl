213ba5b03fffbcf6a7ff78a3585568eff7b43527
Andreas Gruenbacher <agruenba@redhat.com>
2014-04-01 18:17:22 +0530

    richacl: Create-time inheritance
    When a new file is created, it can inherit an acl from its parent
    directory; this is similar to how default acls work in POSIX (draft)
    ACLs.
    
    As with POSIX ACLs, if a file inherits an acl from its parent directory,
    the intersection between the create mode and the permissions granted by
    the inherited acl determines the file masks and file permission bits,
    and the umask is ignored.
    
    Signed-off-by: Andreas Gruenbacher <agruenba@redhat.com>

--- include/linux/richacl.h
+++ include/linux/richacl.h
@@ -293,5 +293,9 @@ extern unsigned int richacl_want_to_mask(unsigned int);
 extern void richacl_compute_max_masks(struct richacl *);
 extern struct richacl *richacl_chmod(struct richacl *, mode_t);
 extern int richacl_permission(struct inode *, const struct richacl *, int);
+extern struct richacl *richacl_inherit(const struct richacl *, int);
 
+/* richacl_inode.c */
+extern struct richacl *richacl_inherit_inode(const struct richacl *,
+					     struct inode *);
 #endif /* __RICHACL_H */
