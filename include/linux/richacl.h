410d49744f16fb757be06a4c2a9e97b9eb760d70
Andreas Gruenbacher <agruenba@redhat.com>
2014-04-01 18:18:38 +0530

    richacl: Check if an acl is equivalent to a file mode
    This function is used to avoid storing richacls if the acl can be computed from
    the file permission bits.
    
    Signed-off-by: Andreas Gruenbacher <agruenba@redhat.com>

--- include/linux/richacl.h
+++ include/linux/richacl.h
@@ -294,6 +294,7 @@ extern void richacl_compute_max_masks(struct richacl *);
 extern struct richacl *richacl_chmod(struct richacl *, mode_t);
 extern int richacl_permission(struct inode *, const struct richacl *, int);
 extern struct richacl *richacl_inherit(const struct richacl *, int);
+extern int richacl_equiv_mode(const struct richacl *, mode_t *);
 
 /* richacl_inode.c */
 extern struct richacl *richacl_inherit_inode(const struct richacl *,
