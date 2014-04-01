c903bf70b36c00cd74de58dad59d9a0144c8c44f
Andreas Gruenbacher <agruenba@redhat.com>
2014-04-01 18:19:48 +0530

    richacl: Automatic Inheritance
    Automatic Inheritance (AI) allows changes to the acl of a directory to
    propagate down to children.
    
    This is mostly implemented in user space: when a process changes the
    permissions of a directory and Automatic Inheritance is enabled for that
    directory, the process must propagate those changes to all children,
    recursively.
    
    The kernel enables this by keeping track of which permissions have been
    inherited at file create time.  In addition, it makes sure that
    permission propagation is turned off when the permissions of a file are
    set explicitly (for example, upon create or chmod).
    
    Automatic Inheritance works as follows:
    
     - When the RICHACL_AUTO_INHERIT flag in the acl of a file is not set,
       the file is not affected by AI.
    
     - When the RICHACL_AUTO_INHERIT flag in the acl of a directory is set
       and a file or subdirectory is created in that directory, the
       inherited acl will have the RICHACL_AUTO_INHERIT flag set, and all
       inherited aces will have the RICHACE_INHERITED_ACE flag set.  This
       allows user space to distinguish between aces which have been
       inherited and aces which have been explicitly added.
    
     - When the RICHACL_PROTECTED acl flag in the acl of a file is set, AI
       will not modify the acl of the file.  This does not affect
       propagation of permissions from the file to its children (if the file
       is a directory).
    
    Linux does not have a way of creating files without setting the file
    permission bits, so all files created inside a directory with
    RICHACL_AUTO_INHERIT set will have the RICHACL_PROTECTED flag set.  This
    effectively disables Automatic Inheritance.
    
    Protocols which support creating files without specifying permissions
    can explicitly clear the RICHACL_PROTECTED flag after creating a file
    and reset the file masks to "undo" applying the create mode; see
    richacl_compute_max_masks().  They should set the RICHACL_DEFAULTED
    flag.  This is a workaround; a mechanism that would allow a process to
    indicate to the kernel to ignore the create mode when there are
    inherited permissions would fix this problem.
    
    Signed-off-by: Andreas Gruenbacher <agruenba@redhat.com>

--- include/linux/richacl.h
+++ include/linux/richacl.h
@@ -53,10 +53,16 @@ struct richacl {
 	     _ace--)
 
 /* a_flags values */
+#define RICHACL_AUTO_INHERIT			0x01
+#define RICHACL_PROTECTED			0x02
+#define RICHACL_DEFAULTED			0x04
 #define RICHACL_WRITE_THROUGH			0x40
 #define RICHACL_MASKED				0x80
 
 #define RICHACL_VALID_FLAGS (					\
+		RICHACL_AUTO_INHERIT |				\
+		RICHACL_PROTECTED |				\
+		RICHACL_DEFAULTED |				\
 		RICHACL_WRITE_THROUGH |				\
 		RICHACL_MASKED)
 
@@ -70,6 +76,7 @@ struct richacl {
 #define RICHACE_NO_PROPAGATE_INHERIT_ACE	0x0004
 #define RICHACE_INHERIT_ONLY_ACE		0x0008
 #define RICHACE_IDENTIFIER_GROUP		0x0040
+#define RICHACE_INHERITED_ACE			0x0080
 #define RICHACE_SPECIAL_WHO			0x4000
 
 #define RICHACE_VALID_FLAGS (					\
@@ -78,13 +85,15 @@ struct richacl {
 	RICHACE_NO_PROPAGATE_INHERIT_ACE |			\
 	RICHACE_INHERIT_ONLY_ACE |				\
 	RICHACE_IDENTIFIER_GROUP |				\
+	RICHACE_INHERITED_ACE |					\
 	RICHACE_SPECIAL_WHO)
 
 #define RICHACE_INHERITANCE_FLAGS (				\
 	RICHACE_FILE_INHERIT_ACE |				\
 	RICHACE_DIRECTORY_INHERIT_ACE |				\
 	RICHACE_NO_PROPAGATE_INHERIT_ACE |			\
-	RICHACE_INHERIT_ONLY_ACE )
+	RICHACE_INHERIT_ONLY_ACE |				\
+	RICHACE_INHERITED_ACE )
 
 /* e_mask bitflags */
 #define RICHACE_READ_DATA			0x00000001
@@ -194,6 +203,18 @@ extern struct richacl *get_cached_richacl_rcu(struct inode *);
 extern void set_cached_richacl(struct inode *, struct richacl *);
 extern void forget_cached_richacl(struct inode *);
 extern struct richacl *get_richacl(struct inode *);
+
+static inline int
+richacl_is_auto_inherit(const struct richacl *acl)
+{
+	return acl->a_flags & RICHACL_AUTO_INHERIT;
+}
+
+static inline int
+richacl_is_protected(const struct richacl *acl)
+{
+	return acl->a_flags & RICHACL_PROTECTED;
+}
 
 /**
  * richace_is_owner  -  check if @ace is an OWNER@ entry
