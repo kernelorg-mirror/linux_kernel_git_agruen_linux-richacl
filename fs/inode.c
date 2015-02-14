53e5f3c5030dfb931c5be451d21cdf0a32695b4a
Andreas Gruenbacher <agruenba@redhat.com>
2015-02-14 19:31:38 +0100

    vfs: Cache base_acl objects in inodes
    POSIX ACLs and richacls are both objects allocated by kmalloc() with a
    reference count which are freed by kfree_rcu().  An inode can either
    cache an access and a default POSIX ACL, or a richacl (richacls do not
    have default acls).  To allow an inode to cache either of the two kinds
    of acls, introduce a new base_acl type and convert i_acl and
    i_default_acl to that type. In most cases, the vfs then doesn't have to
    care which kind of acl an inode caches (if any).
    
    Signed-off-by: Andreas Gruenbacher <agruenba@redhat.com>

--- fs/inode.c
+++ fs/inode.c
@@ -235,9 +235,9 @@ void __destroy_inode(struct inode *inode)
 
 #ifdef CONFIG_FS_POSIX_ACL
 	if (inode->i_acl && inode->i_acl != ACL_NOT_CACHED)
-		posix_acl_release(inode->i_acl);
+		put_base_acl(inode->i_acl);
 	if (inode->i_default_acl && inode->i_default_acl != ACL_NOT_CACHED)
-		posix_acl_release(inode->i_default_acl);
+		put_base_acl(inode->i_default_acl);
 #endif
 	this_cpu_dec(nr_inodes);
 }
