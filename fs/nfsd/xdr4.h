e51878cd608e0f3dedb06e6d76edca0ea1477d76
Andreas Gruenbacher <agruenba@redhat.com>
2015-03-05 12:57:38 +0100

    nfsd: Keep list of acls to dispose of in compoundargs
    We will decode acls in requests into richacls.  Even if unlikely, there
    can be more than one acl in a single request; those richacls need to be
    richacl_put() at the end of the request instead of kfree()d, so keep a
    list of acls in compoundargs for that.
    
    Signed-off-by: Andreas Gruenbacher <agruenba@redhat.com>
    Acked-by: J. Bruce Fields <bfields@redhat.com>

--- fs/nfsd/xdr4.h
+++ fs/nfsd/xdr4.h
@@ -583,6 +583,11 @@ struct svcxdr_tmpbuf {
 	char buf[];
 };
 
+struct svcxdr_richacl {
+	struct svcxdr_richacl *next;
+	struct richacl *acl;
+};
+
 struct nfsd4_compoundargs {
 	/* scratch variables for XDR decode */
 	__be32 *			p;
@@ -592,6 +597,7 @@ struct nfsd4_compoundargs {
 	__be32				tmp[8];
 	__be32 *			tmpp;
 	struct svcxdr_tmpbuf		*to_free;
+	struct svcxdr_richacl		*acls;
 
 	struct svc_rqst			*rqstp;
 
