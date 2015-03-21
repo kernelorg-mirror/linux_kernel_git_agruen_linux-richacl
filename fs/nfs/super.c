1febd1d29fb811c8896373dbd2c6f3361bbeae3a
Andreas Gruenbacher <agruenba@redhat.com>
2015-03-21 23:12:35 +0100

    nfs: Add richacl support
    Add support for the "system.richacl" xattr in nfs.  The existing
    "system.nfs4_acl" xattr on nfs doesn't map user and group names to uids
    and gids; the "system.richacl" xattr does, and only keeps the
    on-the-wire names when there is no mapping.  This allows to copy
    permissions across different file systems.
    
    Signed-off-by: Andreas Gruenbacher <agruenba@redhat.com>

--- fs/nfs/super.c
+++ fs/nfs/super.c
@@ -2316,7 +2316,7 @@ void nfs_fill_super(struct super_block *sb, struct nfs_mount_info *mount_info)
 		/* The VFS shouldn't apply the umask to mode bits. We will do
 		 * so ourselves when necessary.
 		 */
-		sb->s_flags |= MS_POSIXACL;
+		sb->s_flags |= MS_RICHACL;
 		sb->s_time_gran = 1;
 	}
 
@@ -2343,7 +2343,7 @@ void nfs_clone_super(struct super_block *sb, struct nfs_mount_info *mount_info)
 		/* The VFS shouldn't apply the umask to mode bits. We will do
 		 * so ourselves when necessary.
 		 */
-		sb->s_flags |= MS_POSIXACL;
+		sb->s_flags |= MS_RICHACL;
 	}
 
  	nfs_initialise_sb(sb);
