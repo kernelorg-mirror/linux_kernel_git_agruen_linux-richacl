accd81a071131e3632cdd5a8032b2ff343fd76fe
Andreas Gruenbacher <agruenba@redhat.com>
2015-03-21 23:12:35 +0100

    nfs: Add richacl support
    Add support for the "system.richacl" xattr in nfs.  The existing
    "system.nfs4_acl" xattr on nfs doesn't map user and group names to uids
    and gids; the "system.richacl" xattr does, and only keeps the
    on-the-wire names when there is no mapping.  This allows to copy
    permissions across different file systems.
    
    Signed-off-by: Andreas Gruenbacher <agruenba@redhat.com>

--- include/linux/nfs_fs_sb.h
+++ include/linux/nfs_fs_sb.h
@@ -248,5 +248,7 @@ struct nfs_server {
 #define NFS_CAP_LAYOUTSTATS	(1U << 22)
 #define NFS_CAP_CLONE		(1U << 23)
 #define NFS_CAP_COPY		(1U << 24)
+#define NFS_CAP_ALLOW_ACLS	(1U << 25)
+#define NFS_CAP_DENY_ACLS	(1U << 26)
 
 #endif
