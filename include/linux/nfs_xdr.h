f9185d9deff215d7ae78de3dbfde8b8d8f57466a
Andreas Gruenbacher <agruenba@redhat.com>
2015-03-23 12:35:21 +0100

    nfs: Remove unused xdr page offsets in getacl/setacl arguments
    The arguments passed around for getacl and setacl xdr encoding, struct
    nfs_setaclargs and struct nfs_getaclargs, both contain an array of
    pages, an offset into the first page, and the length of the page data.
    The offset is unused as it is always zero; remove it.
    
    Signed-off-by: Andreas Gruenbacher <agruenba@redhat.com>

--- include/linux/nfs_xdr.h
+++ include/linux/nfs_xdr.h
@@ -685,7 +685,6 @@ struct nfs_setaclargs {
 	struct nfs4_sequence_args	seq_args;
 	struct nfs_fh *			fh;
 	size_t				acl_len;
-	unsigned int			acl_pgbase;
 	struct page **			acl_pages;
 };
 
@@ -697,7 +696,6 @@ struct nfs_getaclargs {
 	struct nfs4_sequence_args 	seq_args;
 	struct nfs_fh *			fh;
 	size_t				acl_len;
-	unsigned int			acl_pgbase;
 	struct page **			acl_pages;
 };
 
