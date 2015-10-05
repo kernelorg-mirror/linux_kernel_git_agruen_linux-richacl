833749bd126ddf9d47fa1ba7e0a49a76fc48fccd
Andreas Gruenbacher <agruenba@redhat.com>
2015-10-05 15:52:43 +0200

    vfs: Add get_richacl and set_richacl inode operations
    These operations are similar to the get_acl and set_acl operations for
    POSIX ACLs.  The distinction between access and default ACLs doesn't exist
    for richacls.
    
    Signed-off-by: Andreas Gruenbacher <agruenba@redhat.com>
    Reviewed-by: Steve French <steve.french@primarydata.com>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -1724,6 +1724,7 @@ struct inode_operations {
 	const char * (*get_link) (struct dentry *, struct inode *, struct delayed_call *);
 	int (*permission) (struct inode *, int);
 	struct posix_acl * (*get_acl)(struct inode *, int);
+	struct richacl * (*get_richacl)(struct inode *);
 
 	int (*readlink) (struct dentry *, char __user *,int);
 
@@ -1754,6 +1755,7 @@ struct inode_operations {
 			   umode_t create_mode, int *opened);
 	int (*tmpfile) (struct inode *, struct dentry *, umode_t);
 	int (*set_acl)(struct inode *, struct posix_acl *, int);
+	int (*set_richacl)(struct inode *, struct richacl *);
 } ____cacheline_aligned;
 
 ssize_t rw_copy_check_uvector(int type, const struct iovec __user * uvector,
