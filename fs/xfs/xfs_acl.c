84b1b465e50675d72987d73738ca7965de44a0e1
Andreas Gruenbacher <agruenba@redhat.com>
2015-10-09 18:11:27 +0200

    xfs: Make xfs_set_mode non-static
    Make xfs_set_mode non-static and move it from xfs_acl.c into xfs_inode.c.
    
    Signed-off-by: Andreas Gruenbacher <agruenba@redhat.com>

--- fs/xfs/xfs_acl.c
+++ fs/xfs/xfs_acl.c
@@ -235,24 +235,6 @@ __xfs_set_acl(struct inode *inode, int type, struct posix_acl *acl)
 }
 
 static int
-xfs_set_mode(struct inode *inode, umode_t mode)
-{
-	int error = 0;
-
-	if (mode != inode->i_mode) {
-		struct iattr iattr;
-
-		iattr.ia_valid = ATTR_MODE | ATTR_CTIME;
-		iattr.ia_mode = mode;
-		iattr.ia_ctime = current_fs_time(inode->i_sb);
-
-		error = xfs_setattr_nonsize(XFS_I(inode), &iattr, XFS_ATTR_NOACL);
-	}
-
-	return error;
-}
-
-static int
 xfs_acl_exists(struct inode *inode, unsigned char *name)
 {
 	int len = XFS_ACL_MAX_SIZE(XFS_M(inode->i_sb));
