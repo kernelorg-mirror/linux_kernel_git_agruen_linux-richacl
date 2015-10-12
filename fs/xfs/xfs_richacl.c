dcc499cc47294f4036d5915d12feb8a9daf73719
Andreas Gruenbacher <agruenba@redhat.com>
2015-10-12 14:12:10 +0200

    xfs: Don't allow unmapped identifiers in richacls
    Don't allow acls which contain unmapped identifiers: they are meaningful
    for remote file systems only.
    
    Signed-off-by: Andreas Gruenbacher <agruenba@redhat.com>
    Reviewed-by: Steve French <steve.french@primarydata.com>

--- fs/xfs/xfs_richacl.c
+++ fs/xfs/xfs_richacl.c
@@ -75,6 +75,10 @@ xfs_set_richacl(struct inode *inode, struct richacl *acl)
 	if (!acl)
 		return xfs_remove_richacl(inode);
 
+	/* Don't allow acls with unmapped identifiers. */
+	if (richacl_has_unmapped_identifiers(acl))
+		return -EINVAL;
+
 	if (richacl_equiv_mode(acl, &mode) == 0) {
 		xfs_set_mode(inode, mode);
 		return xfs_remove_richacl(inode);
